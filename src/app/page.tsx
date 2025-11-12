0e12aaccc1a44ae21350e71e807312725002b818
Sushant <sushant@192.168.0.191>
2025-11-12 14:06:37 +0530

    home portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
 import Image from "next/image";
-
+import Hero from '../../public/hero.png'
 export default function Home() {
-  return <div>home</div>
-   
+  return (
+   <div className="flex md:flex-row flex-col gap-10">
+    <div className="basis-1/2 self-center justify-self-center">
+    <h1 className="text-6xl font-bold bg-linear-to-b mb-8 from-green-900 to-white bg-clip-text text-transparent">
+      Better Design for the digital Products.
+    </h1>
+    <p className="text-xl mb-8">Turning your idea into Reality. We bring together the teams from the global tech industry.</p>
+    <button className="bg-green-700 rounded-lg font-bold px-3 py-5">See Our Works</button>
+    </div>
+    <div className="basis-1/2 self-center justify-self-center">
+      <Image src={Hero} alt={""}></Image>
+    </div>
+   </div>
+  );
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import Image from "next/image";
+import Illustrations from "../../../public/illustration.png";
+import Apps from "../../../public/apps.jpg";
+import Websites from "../../../public/websites.jpg";
 function Portfolio() {
   return (
-    <div>Portfolio</div>
-  )
+    <div>
+      <h1 className="text-8xl font-bold mb-5">Our Works</h1>
+      <h1 className="text-3xl mb-5 font-bold">Choose a gallery</h1>
+      <div className="flex gap-4">
+        <div className="basis-1/4 relative">
+          <Image
+            className="h-96 w-72 border-4 object-cover  border-gray-300 "
+            src={Illustrations}
+            alt={""}
+          ></Image>
+           <p className="absolute text-4xl font-extrabold right-12 bottom-3 z-10">Illustrations</p>
+        </div>
+        <div className="basis-1/4 relative">
+          <Image
+            className="h-96 w-72 border-4 object-cover object-left border-gray-300  "
+            src={Websites}
+            alt={""}
+          ></Image>
+          <p className="absolute text-4xl font-extrabold right-12 bottom-3 z-10">Websites</p>
+        </div>
+        <div className="basis-1/4 relative">
+          <Image
+            className="h-96 w-72 border-4 z-0 object-left object-cover border-gray-300 "
+            src={Apps}
+            alt={""}
+          ></Image>
+          <p className="absolute text-4xl font-extrabold right-12 bottom-3 z-10">Applications</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
